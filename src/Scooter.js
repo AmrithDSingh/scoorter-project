60bae6832d8967c5592fbe1f21076eed03b0f5c4
AmrithDSingh <amrithdathsingh04@gmail.com>
2023-02-10 16:30:58 +0000

    Scooter test practice

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,19 @@
 class Scooter{
   // scooter code here
+  static nextSerial = 1;
+  station = null;
+  user = null;
+  serial = 0;
+  charge = 0;
+  isBroken = false;
+
+  constructor() {
+    this.serial = Scooter.nextSerial++;
+    this.station = null;
+    this.user = null;
+    this.charge = 0;
+    this.isBroken = false;
+  }
 }
 
 
--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -1,14 +1,23 @@
-const Scooter = require('../src/Scooter')
-const User = require('../src/User')
+const Scooter = require('../src/Scooter');
+const User = require('../src/User');
 
-//typeof scooter === object
-describe('scooter object', () => {
-  test('does something', () => {
+//typeof scooter === object 
+describe('scooter instance integrity checks', () => {
+  const scooter = new Scooter("Liverpool");
+  test('instance has correct properties', () => {
     // edit this to be a real test!
-    expect(false).toEqual(true);
-  }
-)
-})
+    expect(scooter).toHaveProperty("user", null);
+    expect(scooter).toHaveProperty("charge");
+    expect(scooter).toHaveProperty("serial");
+    expect(typeof scooter.isBroken).toBe("boolean");
+    expect(scooter.station).toBe("Liverpool");
+  });
+
+  test("instance static value incrementing", () =>{
+  const scooter2 = new Scooter("Wirral");
+  expect(scooter2.serial).toBe(scooter.serial + 1);
+});
+});
 
 //Method tests
 describe('scooter methods', () => {
